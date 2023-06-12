7338464896b79ce821b1b5ef161f89bd0ab5320b
AlyafeiAli <theghost970@gmail.com>
2023-06-12 22:12:03 +0400

    progress

--- script.js
+++ script.js
@@ -40,6 +40,25 @@ console.log(ageCalc(2015));
 console.log(ageCalc(1997));
 console.log(ageCalc(1970));
 
+
 // Arrow functions and Ternary Operator
-const ageCalc2 = (year) => year <= new Date().getFullYear() ? new Date().getFullYear() - year : `impossible year. It should be less than or equal ${new Date().getFullYear()}`;
+const ageCalc2 = (year) => year <= new Date().getFullYear() 
+    ? new Date().getFullYear() - year 
+    : `impossible year. It should be less than or equal ${new Date().getFullYear()}`;
 console.log(ageCalc2(2058));
+console.log(ageCalc2(2008));
+
+
+// Arrays
+const myList = [
+    "Abu Dhabi is the capital of UAE",
+    2023,
+    true,
+    "Baka Desu"
+]
+console.log(myList[0]);
+console.log(myList[myList.length - 1]); //last item
+
+const [text, year, isTrue, bakaDesuKa] = myList; // Assigning multiple values at once
+console.log(bakaDesuKa);
+console.log(year);
