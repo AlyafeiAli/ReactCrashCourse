a728d1e5f8fee1ee9be786a53d507602a53d6ab9
AlyafeiAli <theghost970@gmail.com>
2023-06-12 18:45:02 +0400

    Arrow functions and Ternary Operator

--- script.js
+++ script.js
@@ -3,34 +3,43 @@ const form = document.querySelector(".fact-form");
 // btn.addEventListener("click", function(){
 //     console.log("CLICK");
 // });
-btn.addEventListener('click', function(){
-    if(form.classList.contains('hidden')){
+btn.addEventListener('click', function () {
+    if (form.classList.contains('hidden')) {
         form.classList.remove('hidden');
         btn.textContent = "Close";
     }
-    else{
+    else {
         form.classList.add('hidden')
         btn.textContent = "Share a fact";
     }
 })
 
-function ageCalc(year){
-    const currentYear = 2023;
+
+// Normal functions
+function ageCalc(year) {
+    const currentYear = new Date().getFullYear();
     const age = currentYear - year;
-    return age;
+    if (age < 0) {
+        return ("Impossible.")
+    }
+    else if (age < 20) {
+        return ("You're young");
+    }
+
+    else if (age < 30) {
+        return ("You're in the middle");
+    }
+
+    else {
+        return ("You're old");
+    }
 }
 
-const age = ageCalc(1997);
-
-if(age < 20){
-    console.log("You're young");
-}
-
-else if(age < 30){
-    console.log("You're in the middle");
-}
-
-else{
-    console.log("You're old");
-}
+console.log(ageCalc(2028));
+console.log(ageCalc(2015));
 console.log(ageCalc(1997));
+console.log(ageCalc(1970));
+
+// Arrow functions and Ternary Operator
+const ageCalc2 = (year) => year <= new Date().getFullYear() ? new Date().getFullYear() - year : `impossible year. It should be less than or equal ${new Date().getFullYear()}`;
+console.log(ageCalc2(2058));
