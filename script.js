be793d3e7692a679ddd7f7b61d038d70d34fc7a4
AlyafeiAli <theghost970@gmail.com>
2023-06-13 23:16:06 +0400

    fetch, async, and await

--- script.js
+++ script.js
@@ -42,26 +42,40 @@ const factsList = document.querySelector(".facts-list");
 
 // Create DOM elements: Render facts
 factsList.innerHTML = "";
-createFactsList(initialFacts);
+// createFactsList(initialFacts);
+
+// Fetch table from Supabase
+loadFacts();
+async function loadFacts() {
+  const res = await fetch("https://xxxx.supabase.co/rest/v1/facts", {
+    headers: {
+      apikey: "xxxxxueXNkdWttQMc",
+      authorization: "Bearer exxxx24iLxxcxMn0.xxxlI5UuelQMc",
+    },
+  });
+  const data = await res.json();
+  console.log(data);
+  createFactsList(data);
+}
 
 function createFactsList(dataArray) {
   const htmlArray = dataArray.map(
     (fact) => `
-    <li class="fact">
-    <p>
-      ${fact.text}
-      <a
-        class="source"
-        href="${fact.source}"
-        target="_blank"
-        >(Source)</a
+      <li class="fact">
+      <p>
+        ${fact.text}
+        <a
+          class="source"
+          href="${fact.source}"
+          target="_blank"
+          >(Source)</a
+        >
+      </p>
+      <span class="tag" style="background-color: #3b82f6"
+        >${fact.category}</span
       >
-    </p>
-    <span class="tag" style="background-color: #3b82f6"
-      >${fact.category}</span
-    >
-    </li>
-    `
+      </li>
+      `
   );
   factsList.insertAdjacentHTML("afterbegin", htmlArray.join(""));
 }
@@ -80,86 +94,86 @@ btn.addEventListener("click", function () {
 });
 
 /*
-
-// Normal functions
-function ageCalc(year) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - year;
-  if (age < 0) {
-    return "Impossible.";
-  } else if (age < 20) {
-    return "You're young";
-  } else if (age < 30) {
-    return "You're in the middle";
-  } else {
-    return "You're old";
+  
+  // Normal functions
+  function ageCalc(year) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - year;
+    if (age < 0) {
+      return "Impossible.";
+    } else if (age < 20) {
+      return "You're young";
+    } else if (age < 30) {
+      return "You're in the middle";
+    } else {
+      return "You're old";
+    }
   }
-}
-
-console.log(ageCalc(2028));
-console.log(ageCalc(2015));
-console.log(ageCalc(1997));
-console.log(ageCalc(1970));
-
-// Arrow functions and Ternary Operator
-const ageCalc2 = (year) =>
-  year <= new Date().getFullYear()
-    ? new Date().getFullYear() - year
-    : `impossible year. It should be less than or equal ${new Date().getFullYear()}`;
-console.log(ageCalc2(2058));
-console.log(ageCalc2(2008));
-
-// Arrays
-const myList = ["Abu Dhabi is the capital of UAE", 2023, true, "Baka Desu"];
-console.log(myList[0]);
-console.log(myList[myList.length - 1]); //last item
-
-const [text, year, isTrue, bakaDesuKa] = myList; // Assigning multiple values at once
-console.log(bakaDesuKa);
-console.log(year);
-
-// Objects / Dictionaries
-const myObj = {
-  text: "Abu Dhabi is the capital of UAE",
-  category: "fact",
-  myYear: 2023,
-  isCorrect: true,
-  createSummary: function () {
-    return `this fact ${this.text} was created in ${this.myYear} and is ${this.isCorrect}`;
-  },
-};
-console.log(myObj.text);
-console.log(myObj["myYear"]);
-
-const { category, isCorrect } = myObj;
-console.log(category);
-console.log(myObj.createSummary());
-
-// Looping over arrays
-const numArray = [2, 4, 6, 8];
-numArray.forEach(function (i) {
-  console.log(i);
-});
-
-// Map method
-const times10 = numArray.map((i) => i * 10); // Same but in arrow function
-// const times10 = numArray.map(function (i) {
-//   return i * 10;
-// });
-console.log(times10);
-
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
-
-const stringOnly = CATEGORIES.map((i) => i.name); // retunr names only
-console.log(stringOnly);
-
-*/
+  
+  console.log(ageCalc(2028));
+  console.log(ageCalc(2015));
+  console.log(ageCalc(1997));
+  console.log(ageCalc(1970));
+  
+  // Arrow functions and Ternary Operator
+  const ageCalc2 = (year) =>
+    year <= new Date().getFullYear()
+      ? new Date().getFullYear() - year
+      : `impossible year. It should be less than or equal ${new Date().getFullYear()}`;
+  console.log(ageCalc2(2058));
+  console.log(ageCalc2(2008));
+  
+  // Arrays
+  const myList = ["Abu Dhabi is the capital of UAE", 2023, true, "Baka Desu"];
+  console.log(myList[0]);
+  console.log(myList[myList.length - 1]); //last item
+  
+  const [text, year, isTrue, bakaDesuKa] = myList; // Assigning multiple values at once
+  console.log(bakaDesuKa);
+  console.log(year);
+  
+  // Objects / Dictionaries
+  const myObj = {
+    text: "Abu Dhabi is the capital of UAE",
+    category: "fact",
+    myYear: 2023,
+    isCorrect: true,
+    createSummary: function () {
+      return `this fact ${this.text} was created in ${this.myYear} and is ${this.isCorrect}`;
+    },
+  };
+  console.log(myObj.text);
+  console.log(myObj["myYear"]);
+  
+  const { category, isCorrect } = myObj;
+  console.log(category);
+  console.log(myObj.createSummary());
+  
+  // Looping over arrays
+  const numArray = [2, 4, 6, 8];
+  numArray.forEach(function (i) {
+    console.log(i);
+  });
+  
+  // Map method
+  const times10 = numArray.map((i) => i * 10); // Same but in arrow function
+  // const times10 = numArray.map(function (i) {
+  //   return i * 10;
+  // });
+  console.log(times10);
+  
+  const CATEGORIES = [
+    { name: "technology", color: "#3b82f6" },
+    { name: "science", color: "#16a34a" },
+    { name: "finance", color: "#ef4444" },
+    { name: "society", color: "#eab308" },
+    { name: "entertainment", color: "#db2777" },
+    { name: "health", color: "#14b8a6" },
+    { name: "history", color: "#f97316" },
+    { name: "news", color: "#8b5cf6" },
+  ];
+  
+  const stringOnly = CATEGORIES.map((i) => i.name); // retunr names only
+  console.log(stringOnly);
+  
+  */
